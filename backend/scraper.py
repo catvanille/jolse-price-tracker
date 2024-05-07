1a502b54ce5119675bc8d82c061a94a8df84f09e
Hangyul Yi <hangyulyi3@gmail.com>
2024-05-07 14:53:18 -0600

    add products.csv to store data

--- backend/scraper.py
+++ backend/scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import smtplib
+import pandas as pd
 URL = 'https://jolse.com/product/beauty-of-joseon-relief-sun-rice-probiotics-50ml/46958/category/1/display/22/'
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
@@ -39,7 +40,12 @@ content = driver.page_source
 soup = BeautifulSoup(content, features="html.parser")
 
 for element in soup.findAll('div', attrs={'class': 'description'}):
-   product = element.find('strong', attrs={'class': 'name'}).text.strip()
-   price = element.find('ul', class_='xans-element-').find('li', class_='xans-record-').find('span').text.strip()
-   print("name: ", product)
-   print("price: ", price)
+    product = element.find('strong', attrs={'class': 'name'}).text.strip()
+    price = element.find('ul', class_='xans-element-').find('li', class_='xans-record-').find('span').text.strip()
+
+    products.append(product)
+    prices.append(price)
+
+# add data to products.csv
+df = pd.DataFrame({'Product': products, 'Price': prices})
+df.to_csv('products.csv', index=False, encoding='utf-8')
