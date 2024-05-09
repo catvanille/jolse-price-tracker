0051bf7d0940eae2a1f97872402879b6b8a9f6fd
catvanille <112831930+catvanille@users.noreply.github.com>
2024-05-09 13:29:48 -0600

    iterate through pages

--- backend/scraper.py
+++ backend/scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import smtplib
 import pandas as pd
-URL = 'https://jolse.com/category/skincare/1018/'
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
 
@@ -19,17 +18,20 @@ def check_price():
         else:
             links.append(link)
         product = element.find('strong', attrs={'class': 'name'}).text.strip()[15:]
-        #price = element.find('ul', class_='xans-element-').find('li', class_='xans-record-').find('span').text.strip()
         price = element.find('ul', attrs={'class': 'xans-element- xans-product xans-product-listitem spec'}).text.strip()[10:]
-        old_price, new_price = price.split('USD ')
+        if 'USD ' in price:
+            old_price, new_price = price.split('USD ')
+        else:
+            old_price = price
+            new_price = price
         products.append(product)
         oldprices.append(float(old_price))
         newprices.append(float(new_price))
-        if element.find('div', attrs={'class': 'icon'}) == True:
+        percentoff.append(round((float(old_price) - float(new_price)) / float(old_price) * 100),2)
+        if 'src="/web/upload/icon_201906191605023500.jpg' in element.find('div', attrs={'class': 'icon'}).text.strip() == True:
             stock.append(0)
         else:
             stock.append(1)
-
         
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -37,25 +39,28 @@ def send_mail():
     server.starttls()
     server.ehlo()
 
-def check_duplicates(seq):
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not(x in seen or seen_add(x))]
+# def check_duplicates(seq):
+#     seen = set()
+#     seen_add = seen.add
+#     return [x for x in seq if not(x in seen or seen_add(x))]
 
 products = []
 oldprices = []
 newprices = []
+percentoff = []
 links = []
 stock = []
-check_price()
+for i in range(1, 86):
+    URL = 'https://jolse.com/category/skincare/1018/' + '?page=' + str(i)
+    check_price()
 
-# remove duplicates
-n_products = check_duplicates(products)
-n_oldprices = check_duplicates(oldprices)
-n_newprices = check_duplicates(newprices)
+# # remove duplicates
+# n_products = check_duplicates(products)
+# n_oldprices = check_duplicates(oldprices)
+# n_newprices = check_duplicates(newprices)
 
 # add data to products.csv
-df = pd.DataFrame({'Product': products, 'Old Price': oldprices, 'New Price': newprices, 'Link': links, 'Stock': stock})
+df = pd.DataFrame({'Product': products, 'Old Price': oldprices, 'New Price': newprices, 'Discount': percentoff, 'Link': links, 'Stock': stock})
 df.to_csv('products.csv', index=False, encoding='utf-8')
 
 
